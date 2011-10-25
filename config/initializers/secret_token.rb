6c24ad699babd89fffbddb7a9c6c8130786d14a1
Ivan Kasatenko <sky.31338@gmail.com>
2011-10-25 11:54:22 +0400

    Empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EventedBlog::Application.config.secret_token = '8ebcbf0ebc55988c1d3ba4edd0229d6ef7462c257e7d99ea07c9730b2fd14b0ed8c7e26fbd51948e6f535801211beaf454af8fd6e169f0ac573cd7a0fa424f2f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+EventedBlog::Application.config.session_store :cookie_store, key: '_EventedBlog_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# EventedBlog::Application.config.session_store :active_record_store
